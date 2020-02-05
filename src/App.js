df302eafa013edc2370a1a557c59e290309a71f1
Shubham Kumar Ojha <shubhamkojha@Shubhams-MacBook-Pro.local>
2020-02-05 10:10:38 +0530

    basci user form completed

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import UserForm from './UserForm.js';
+import './App.css';
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> <UserForm /> </div>
+
+    );
+  }
+}
+export default App;
---
+++ src/utils/API.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://sih-2020-26518.firebaseapp.com/",
+  responseType: "json",
+  
+});
