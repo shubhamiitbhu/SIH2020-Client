90b2b385e73cffb4f73d3936c680a2c82b0053de
shubhamiitbhu <shubhamk.ojha.civ16@iitbhu.ac.in>
2020-03-21 14:00:14 +0530

    train name bug fixed

--- src/Components/DirectTrains.js
+++ src/Components/DirectTrains.js
@@ -6,7 +6,7 @@ const DirectTrains = ({train, index}) =>
 {
     console.log(train);
     const trainNumber = train.number;
-    const trainName = train.name[0];
+    const trainName = train.name!==undefined?train.name[0]:null;
     return (
     <li id={index}>{trainNumber} - {trainName}</li>
     );
