fb9b93b8bb52640599acff4b83249447b08c39b0
TANUJ DWIVEDI <tanuj.dwivedi.civ16@itbhu.ac.in>
2020-07-08 13:38:03 +0530

    bot detection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React from "react";
 
-import UserForm from './Components//UserForm.js';
-import './App.css';
-
+import UserForm from "./Components//UserForm.js";
+import BotDetector from "./Helpers/bot-detector.js";
+import "./App.css";
 
 class App extends React.Component {
+  componentDidUpdate() {
+    var callback = function (result) {
+      if (result.isBot) {
+        alert("You are a fucking bot!");
+      } else {
+        alert("Welcome user!");
+      }
+    };
+    var botDetector = new BotDetector({
+      timeout: 1000,
+      callback: callback,
+    });
+  }
   render() {
     return (
-      <div> <UserForm /> </div>
-
+      <div>
+        {" "}
+        <UserForm />{" "}
+      </div>
     );
   }
 }
